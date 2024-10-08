5153e8a275df6cb0c00c862023daf85d980b85ed
加藤和哉 <soanevalcke@hotmail.co.jp>
2024-10-08 19:23:30 +0900

    会員側の認証を実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->id()->comment('ID');
+            $table->string('name')->comment('ユーザー名（漢字）');
+            $table->string('kana')->comment('ユーザー名（フリガナ）');
+            $table->string('email')->unique()->comment('メールアドレス');
+            $table->timestamp('email_verified_at')->nullable()->comment('メールアドレス認証の完了・未完了');
+            $table->string('password')->comment('パスワード');
+            $table->string('postal_code')->comment('郵便番号');
+            $table->string('address')->comment('住所');
+            $table->string('phone_number')->comment('電話番号');
+            $table->date('birthday')->nullable()->comment('生年月日');
+            $table->string('occupation')->nullable()->comment('職業');
+            $table->rememberToken()->comment('ログイン状態を保持するためのトークン（秘密の文字列）');
             $table->timestamps();
         });
     }
